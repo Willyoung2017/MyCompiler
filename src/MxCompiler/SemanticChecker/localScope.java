58ac34d0d0da60d0218d53a827c9da6ba1f7e796
willyoung2017 <986645906@qq.com>
2018-04-29 09:49:24 +0800

    semantic.bash update

---
+++ src/MxCompiler/SemanticChecker/localScope.java
@@ -0,0 +1,27 @@
+package SemanticChecker;
+
+import MxCompiler.Exception.compilationError;
+import MxCompiler.Ast.location;
+import MxCompiler.Ast.astNode;
+
+public class localScope extends scope {
+    protected  scope parent;
+
+    public localScope(){
+        parent = null;
+    }
+
+    public localScope(scope parent){
+        this.parent = parent;
+    }
+
+    public astNode get(compilationError error, String name, location loc){
+        astNode var = entities.get(name);
+        if(var != null){
+            return var;
+        }
+        else {
+            return parent.get(error, name, loc);
+        }
+    }
+}
